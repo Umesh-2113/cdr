065b855702937ae9b7c20004d532400eb268e9b2
Umesh <umeshsolanki7020@gmail.com>
2023-07-22 12:33:37 +0530

    woww

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,62 +1,38 @@
-const baseUrl = process.env.BASE_URL
+const baseUrl = 'https://cdr-rouge.vercel.app'; // Update with your actual base URL
 
-export const getData = async (url, token) =>{
-    const res = await fetch(`https://cdr-rouge.vercel.app/api/${url}`, {
-        method: 'GET',
-        headers: {
-            'Authorization': token 
-        }
-    })
-    const data = await res.json()
-    return data
-}
+const fetchData = async (url, method, token, body = null) => {
+  try {
+    const requestOptions = {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token,
+      },
+      body: body ? JSON.stringify(body) : null,
+    };
 
-export const postData = async (url, post, token) =>{
-    const res = await fetch(`https://cdr-rouge.vercel.app/api/${url}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type':'application/json',
-            'Authorization': token 
-        },
-        body: JSON.stringify(post)
-    })
-    const data = await res.json()
-    return data
-}
+    const response = await fetch(`${baseUrl}/api/${url}`, requestOptions);
+    const data = await response.json();
 
-export const putData = async (url, post, token) =>{
-    const res = await fetch(`https://cdr-rouge.vercel.app/api/${url}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type':'application/json',
-            'Authorization': token 
-        },
-        body: JSON.stringify(post)
-    })
-    const data = await res.json()
-    return data
-}
+    if (!response.ok) {
+      // If the response status is not in the 2xx range, throw an error with the error message.
+      throw new Error(data.message || 'Something went wrong.');
+    }
 
-export const patchData = async (url, post, token) =>{
-    const res = await fetch(`https://cdr-rouge.vercel.app/api/${url}`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type':'application/json',
-            'Authorization': token 
-        },
-        body: JSON.stringify(post)
-    })
-    const data = await res.json()
-    return data
-}
-export const deleteData = async (url, token) =>{
-    const res = await fetch(`https://cdr-rouge.vercel.app/api/${url}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type':'application/json',
-            'Authorization': token 
-        },
-    })
-    const data = await res.json()
-    return data
-}
+    return data;
+  } catch (error) {
+    // Handle any errors that might occur during the fetch.
+    console.error('Fetch Error:', error.message);
+    throw error;
+  }
+};
+
+export const getData = async (url, token) => fetchData(url, 'GET', token);
+
+export const postData = async (url, data, token) => fetchData(url, 'POST', token, data);
+
+export const putData = async (url, data, token) => fetchData(url, 'PUT', token, data);
+
+export const patchData = async (url, data, token) => fetchData(url, 'PATCH', token, data);
+
+export const deleteData = async (url, token) => fetchData(url, 'DELETE', token);
